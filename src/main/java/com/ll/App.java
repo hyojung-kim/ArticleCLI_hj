1dcce756bb58d41dd2ac86b3fdc1c82a2d377d03
ZarKljh <zarkljh@gmail.com>
2025-07-18 15:43:36 +0900

    command declare in App class

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,10 +4,11 @@ import com.ll.article.ArticleController;
 import com.ll.db.DBConnection;
 import com.ll.system.SystemController;
 
+
 public class App {
     ArticleController articleController;
     SystemController systemController;
-    Container container;
+
 
     App(){
         DBConnection.DB_NAME = "hjDB";
@@ -19,10 +20,10 @@ public class App {
         Container.getDBConnection().connect();
         articleController = new ArticleController();
         systemController = new SystemController();
-        container = new Container();
+
     }
     public void run(){
 
-        System.out.println();
+        String command = Container.getSc().nextLine().trim();
     }
 }
